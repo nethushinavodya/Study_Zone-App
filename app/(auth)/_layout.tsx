3f5077f1e5c6bca7415b243229ebbc03dbd7ff0c
nethushinavodya <nethushiperera@gmail.com>
2026-01-31 12:05:52 +0530

    Add AuthLayout component for authentication screens

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const AuthLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+      }}
+    >
+      <Stack.Screen name="welcome" options={{ title: "Welcome" }} />
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="register" options={{ title: "Register" }} />
+    </Stack>
+  );
+};
+
+export default AuthLayout;
