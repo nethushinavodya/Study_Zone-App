77b520bec41b6247a44552d88411b232dc5bd742
nethushinavodya <nethushiperera@gmail.com>
2026-02-10 12:36:12 +0530

    Refactor layout to include lock screen functionality based on authentication state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,20 +6,28 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import "../global.css";
+import { useAuth } from '@/hooks/useAuth';
+import LockScreen from '@/components/LockScreen';
 
 // App.tsx ->
-const RootLayout = () => {
+const RootLayoutInner = () => {
   const insets = useSafeAreaInsets();
+  const { locked } = useAuth();
 
-  console.log(insets);
+  return (
+    <View style={{ marginTop: insets.top, flex: 1 }}>
+      <Slot />
+      {locked ? <LockScreen /> : null}
+    </View>
+  );
+};
 
+const RootLayout = () => {
   return (
     // <SafeAreaView className="flex-1">
     <LoaderProvider>
       <AuthProvider>
-        <View style={{ marginTop: insets.top, flex: 1 }}>
-          <Slot />
-        </View>
+        <RootLayoutInner />
       </AuthProvider>
       <Toast />
     </LoaderProvider>
